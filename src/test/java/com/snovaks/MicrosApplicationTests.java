e7f395b8dd6f32dc8c8992dc25f4ca55db711d4c
snovaks <szymonnowak8@gmail.com>
2019-06-08 10:43:05 +0200

    Initial commit

---
+++ src/test/java/com/snovaks/MicrosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.snovaks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicrosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
