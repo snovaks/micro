af9d46774894d8a825ff314cf7672b6381c18553
snovaks <szymonnowak8@gmail.com>
2019-06-08 17:02:39 +0200

    Added ImageCrawler class

---
+++ src/main/java/com/snovaks/utilities/ImageCrawler.java
@@ -0,0 +1,43 @@
+package com.snovaks.utilities;
+
+import java.util.regex.Pattern;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.BinaryParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public class ImageCrawler extends WebCrawler {
+
+	private static final Pattern EXCLUSIONS 
+		= Pattern.compile(".*(\\.(css|js|xml|gif|png|mp3|mp4|zip|gz|pdf))$");
+	
+	private static final Pattern IMG_PATTERNS = Pattern.compile(".*(\\.(jpg|jpeg))$");
+	
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+		String urlString = url.getURL().toLowerCase();
+		
+		if(EXCLUSIONS.matcher(urlString).matches()) {
+			return false;
+		}
+		
+		if(IMG_PATTERNS.matcher(urlString).matches()
+				|| urlString.startsWith(referringPage.getWebURL().getURL())) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		if(IMG_PATTERNS.matcher(url).matches()
+				&& page.getParseData() instanceof BinaryParseData) {
+			String extension = url.substring(url.lastIndexOf("."));
+			int contentLength = page.getContentData().length;
+		}
+	}
+	
+}
