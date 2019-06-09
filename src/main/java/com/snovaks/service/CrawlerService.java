1c76ce9edc5e093bed89988dc14fd20251ff3a5d
snovaks <szymonnowak8@gmail.com>
2019-06-10 00:59:33 +0200

    Added Crawl configuration

--- src/main/java/com/snovaks/service/CrawlerService.java
+++ src/main/java/com/snovaks/service/CrawlerService.java
@@ -1,43 +1,77 @@
 package com.snovaks.service;
 
-import javax.annotation.Resource;
+import java.io.File;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import com.snovaks.domain.SearchRequest;
 import com.snovaks.utilities.HtmlCrawler;
 import com.snovaks.utilities.ImageCrawler;
 
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 @Service
 public class CrawlerService {
-
-	private CrawlController textCrawlController;
-	private CrawlController imageCrawlController;
 	
-	public void searchPlainText(SearchRequest searchRequest) {
-		String url = searchRequest.getUrl();
-		textCrawlController.addSeed(url);
-		textCrawlController.startNonBlocking(HtmlCrawler::new, 10);
-		textCrawlController.waitUntilFinish();
+	public void searchText(SearchRequest searchRequest) throws Exception {
+		
+		File crawlStorageBase = new File("src/test/resources/crawler4j/html");
+		String domainName = getDomainName(searchRequest.getUrl());
+		
+		CrawlConfig htmlConfig = new CrawlConfig();
+		htmlConfig.setCrawlStorageFolder(new File(crawlStorageBase, domainName).getAbsolutePath());
+		htmlConfig.setMaxPagesToFetch(100);
+		htmlConfig.setResumableCrawling(true);
+		PageFetcher pageFetcherHtml = new PageFetcher(htmlConfig);
+		
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(
+				robotstxtConfig, pageFetcherHtml);
+		
+		CrawlController htmlController = new CrawlController(
+				htmlConfig, pageFetcherHtml, robotstxtServer);
+		
+		htmlController.addSeed(searchRequest.getUrl());
+		htmlController.startNonBlocking(HtmlCrawler::new, 10);
+		htmlController.waitUntilFinish();
 	}
 	
-	public void searchImages(SearchRequest searchRequest) {
-		String url = searchRequest.getUrl();
-		imageCrawlController.addSeed(url);
-		imageCrawlController.startNonBlocking(ImageCrawler::new, 10);
-		imageCrawlController.waitUntilFinish();
+	public void searchImages(SearchRequest searchRequest) throws Exception {
+		
+		File crawlStorageBase = new File("src/test/resources/crawler4j/image");
+		String domainName = getDomainName(searchRequest.getUrl());
+		
+		CrawlConfig htmlConfig = new CrawlConfig();
+		htmlConfig.setCrawlStorageFolder(new File(crawlStorageBase, domainName).getAbsolutePath());
+		htmlConfig.setMaxPagesToFetch(100);
+		htmlConfig.setResumableCrawling(true);
+		htmlConfig.setIncludeBinaryContentInCrawling(true);
+		PageFetcher pageFetcherHtml = new PageFetcher(htmlConfig);
+		
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(
+				robotstxtConfig, pageFetcherHtml);
+		
+		CrawlController imageController = new CrawlController(
+				htmlConfig, pageFetcherHtml, robotstxtServer);
+		
+		imageController.addSeed(searchRequest.getUrl());
+		imageController.startNonBlocking(ImageCrawler::new, 10);
+		imageController.waitUntilFinish();
 	}
-
-	@Resource(name = "textCrawlController")
-	public void setTextCrawlController(CrawlController textCrawlController) {
-		this.textCrawlController = textCrawlController;
-	}
-
-	@Resource(name = "imageCrawlController")
-	public void setImageCrawlController(CrawlController imageCrawlController) {
-		this.imageCrawlController = imageCrawlController;
+	
+	private String getDomainName(String url) {
+		int indexSecondSlash = StringUtils.ordinalIndexOf(url, "/", 2);
+		int indexThirdSlash = StringUtils.ordinalIndexOf(url, "/", 3);
+		String domainName = url.substring(indexSecondSlash, indexThirdSlash);
+		domainName = domainName.replace(".", "-");
+		domainName = domainName.replace("/", "-");
+		return domainName;
 	}
 	
 }
