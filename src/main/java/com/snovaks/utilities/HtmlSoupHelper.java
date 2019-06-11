f663dbf4b8606d07cfb34669f70f65f6d4f4e5f1
snovaks <szymonnowak8@gmail.com>
2019-06-11 23:55:46 +0200

    Fixed htmlHelper

--- src/main/java/com/snovaks/utilities/HtmlSoupHelper.java
+++ src/main/java/com/snovaks/utilities/HtmlSoupHelper.java
@@ -1,79 +1,28 @@
 package com.snovaks.utilities;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
-import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class HtmlSoupHelper {
 
-	private static final String TEXT_DESTINATION_FOLDER = "./texts";
-	private static final Logger logger = LoggerFactory.getLogger(HtmlSoupHelper.class);
-	
-	public void connectGetAndDownloadTheDocument(String stringURL) {
-		try {
-			Document document = Jsoup.connect(stringURL).timeout(10 * 1000).get();
-			String documentText = document.body().text();
-			String documentName = document.baseUri();
-			saveTheText(documentText, documentName);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void saveTheText(String textContent, String stringElementURL) throws IOException {
-		File file = new File(TEXT_DESTINATION_FOLDER);
-		boolean fileExists = file.exists();
-		if(!fileExists) {
-			fileExists = file.mkdir();
-		}
+	public File connectGetAndDownloadText(String stringURL) throws IOException {
 		
-		int indexSecondSlash = StringUtils.ordinalIndexOf(stringElementURL, "/", 2);
-		int indexThirdSlash = StringUtils.ordinalIndexOf(stringElementURL, "/", 3);
-		String domainName = stringElementURL.substring(indexSecondSlash, indexThirdSlash);
-		domainName = domainName.replace(".", "-");
+		Document document = Jsoup.connect(stringURL).timeout(10 * 1000).get();
+		String documentText = document.body().text();
+		byte[] bytes = documentText.getBytes(StandardCharsets.UTF_8);
+		File file = File.createTempFile("texts", ".txt");
 		
-		String domainFilePath = TEXT_DESTINATION_FOLDER + "/" + domainName;
-		File domainFile = new File(domainFilePath);
+		OutputStream outputStream = new FileOutputStream(file);
+		outputStream.write(bytes);
+		outputStream.close();
 		
-		boolean domainFileExists = domainFile.exists();
-		if(!domainFileExists) {
-			domainFileExists = domainFile.mkdir();
-		}
-		
-		String textFileName = calculateTextFileName(stringElementURL);
-		String finalTextFileName = domainFilePath + "/" + textFileName + ".txt";
-		
-		try (
-			var fileWriter = new FileWriter(finalTextFileName);
-			var writer = new BufferedWriter(fileWriter);
-		) {
-			writer.write(textContent);
-			writer.newLine();
-			logger.info("File has been saved " + finalTextFileName);
-		} catch (IOException e) {
-			logger.warn("Error during saving", e);
-		}	
-	}
-	
-	private String calculateTextFileName(String stringURL) {
-		String textFileName = stringURL;
-		int stringLength = stringURL.length();
-		char lastChar = stringURL.charAt(stringLength - 1);
-		
-		if(lastChar == '/') {
-			StringBuilder sb = new StringBuilder(stringURL);
-			sb.deleteCharAt(stringLength - 1);
-			textFileName = sb.toString();
-		}
-		
-		return textFileName.substring(textFileName.lastIndexOf("/") + 1);
+		return file;
 	}
 	
 }
