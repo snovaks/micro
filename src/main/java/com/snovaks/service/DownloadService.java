e1242f0821489a1c3941dc96d56049a1a3fac181
snovaks <szymonnowak8@gmail.com>
2019-06-12 00:00:09 +0200

    Added initialize method

--- src/main/java/com/snovaks/service/DownloadService.java
+++ src/main/java/com/snovaks/service/DownloadService.java
@@ -8,12 +8,10 @@ import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.snovaks.entity.CrawlEntity;
-import com.snovaks.repository.CrawlContentEntityRepository;
 import com.snovaks.repository.CrawlEntityRepository;
 import com.snovaks.specification.CrawlEntitySpecification;
 
@@ -22,29 +20,25 @@ import com.snovaks.specification.CrawlEntitySpecification;
 public class DownloadService {
 
 	private CrawlEntityRepository crawlEntityRepository;
-	private CrawlContentEntityRepository crawlContentEntityRepository;
 	
 	@Autowired
-	public DownloadService(CrawlEntityRepository crawlEntityRepository, CrawlContentEntityRepository crawlContentEntityRepository) {
+	public DownloadService(CrawlEntityRepository crawlEntityRepository) {
 		this.crawlEntityRepository = crawlEntityRepository;
-		this.crawlContentEntityRepository = crawlContentEntityRepository;
 	}	
 		
+	public Page<CrawlEntity> findCrawls(String url, int numberOfSite, int sizeOfSite) {
+		PageRequest pageable = new PageRequest(numberOfSite, sizeOfSite);
+		Specification<CrawlEntity> spec = CrawlEntitySpecification.withURL(url);
+		return crawlEntityRepository.findAll(spec, pageable);
+	}
 		
-		public Page<CrawlEntity> findCrawls(String url, int numberOfSite, int sizeOfSite) {
-			PageRequest pageable = new PageRequest(numberOfSite, sizeOfSite);
-			Specification<CrawlEntity> spec = CrawlEntitySpecification.withURL(url);
-			return crawlEntityRepository.findAll(spec, pageable);
+	public Page<CrawlEntity> findCrawlsAndInitializeChildren(String url, int numberOfSite, int sizeOfSite) {
+		Page<CrawlEntity> page = findCrawls(url, numberOfSite, sizeOfSite);
+		List<CrawlEntity> list = page.getContent();
+		for(CrawlEntity ce : list) {
+			Hibernate.initialize(ce.getCrawlContentEntities());
 		}
-	
-		
-		public Page<CrawlEntity> findCrawlsAndInitializeChildren(String url, int numberOfSite, int sizeOfSite) {
-			Page<CrawlEntity> page = findCrawls(url, numberOfSite, sizeOfSite);
-			List<CrawlEntity> list = page.getContent();
-			for(CrawlEntity ce : list) {
-				Hibernate.initialize(ce.getCrawlContentEntities());
-			}
-			return page;
-		}
+		return page;
+	}
 	
 }
